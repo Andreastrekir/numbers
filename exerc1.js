7a395ec1b0e7cfefdf3da505e28801aa267ac5a7
andreastrekir <astrekir@gmail.com>
2021-04-20 17:33:43 +0300

    Second commit

---
+++ exerc1.js
@@ -0,0 +1,21 @@
+var number = 5
+
+console.log(number + "34");
+console.log(number - "4");
+console.log(10 % number);
+console.log(number % 10);
+
+var name= "Java"
+var surname= "Script"
+
+console.log(name + " " + surname);
+console.log(" " + " ");
+console.log(" " + 0);
+
+console.log(true + true);
+console.log(true + false);
+console.log(false + true);
+console.log(false - true);
+console.log(3 - 4);
+console.log("Bob" - "Bill");
+
